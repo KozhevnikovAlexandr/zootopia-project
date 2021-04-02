76558bbd06d28dd5685f9a3a48091a0c3afd95d0
Alphiruwa <59170859+Alphiruwa@users.noreply.github.com>
2021-04-03 02:34:55 +0500

    Add files via upload

---
+++ Webcam photomaker BETA.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+import cv2, time, zipfile, os, smtplib
+
+stream = cv2.VideoCapture(0)
+stream.set(cv2.CAP_PROP_FPS, 24) 
+stream.set(cv2.CAP_PROP_FRAME_WIDTH, 1024)
+stream.set(cv2.CAP_PROP_FRAME_HEIGHT, 768) 
+
+os.chdir('C:\\')
+        
+n = 0
+
+key = input('Чтобы сделать фото, введи «1». \nУ тебя будет 10 секунд на то, чтобы принять нужную позу. \nВнимание! Чтобы фотографии сохранились, обязательно заверши программу командой «2»\n')
+zip = zipfile.ZipFile('Фотосессия.zip', 'w')
+
+while (stream.isOpened()):
+
+    if key == '1':
+        print('\nПредставь, что ты фотомодель — приготовься.')
+        for i in range(10):
+            time.sleep(1)
+            print(f'{10-i}...')
+        status, photo = stream.read()
+        print('Попался, красавчик!')
+        cv2.imshow('SEXUALITY = 100%', photo)
+        if cv2.waitKey(10) == 27:
+            break    
+        cv2.imwrite(f'{n}.jpg', photo)
+        json = open(f'{n}.json', 'w+')
+        json.write('[1, [0, 0, 0, 0]]')
+        json.close()
+        zip.write(f'{n}.jpg')
+        os.remove(f'C:\{n}.jpg')        
+        zip.write(f'{n}.json')
+        os.remove(f'C:\{n}.json')        
+        n+=1        
+        key = input('\n- Снова нажми «1», чтобы сделать ещё фото; \n- Нажми «2», чтобы закончить и сохранить фотосессию.\n')
+
+    elif key == '2':
+        zip.close()    
+        print('\nСохранено!\nИщи свои фото по адресу C:/Фотосессия.zip')
+        
+        break
+        
+    elif key == '3':
+        print('\nСворачиваемся...\n')
+        break        
+
+    else:
+        key = input('\nНе-а, только «1», «2» или «3»\n')       
+        
+stream.release()
+cv2.destroyAllWindows()        
+
+input()
